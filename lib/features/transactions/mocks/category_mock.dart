f3a129f044db58fc7d5b8cb0c62fba1307a9deb1
Tayane Milagres <devtaay@gmail.com>
2026-08-03 22:53:38 -0300

    feat: add mock de categorias

---
+++ lib/features/transactions/mocks/category_mock.dart
@@ -0,0 +1,37 @@
+import '../models/category.dart';
+
+final mockCategories = [
+  Category(
+    id: 'alimentacao',
+    label: 'Alimentação',
+  ),
+  Category(
+    id: 'transporte',
+    label: 'Transporte',
+  ),
+  Category(
+    id: 'moradia',
+    label: 'Moradia',
+  ),
+  Category(
+    id: 'renda',
+    label: 'Renda',
+  ),
+  Category(
+    id: 'transferencia',
+    label: 'Transferência',
+  ),
+  Category(
+    id: 'lazer',
+    label: 'Lazer',
+
+  ),
+  Category(
+    id: 'saude',
+    label: 'Saúde',
+  ),
+  Category(
+    id: 'educacao',
+    label: 'Educação',
+  ),
+];
